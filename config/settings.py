bc3e19c009e5da075e3e47854d09053482357f32
hhongjj <hhongjj3@gmail.com>
2021-11-25 13:34:47 +0900

    settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'https://doppelganger-movie.herokuapp.com',
+    '127.0.0.1'
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
